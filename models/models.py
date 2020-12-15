02c05ce2ebc9f6aa8b1e8e174e779a0dba1f19c8
Roman Shveda <rshveda@gmail.com>
2020-12-15 10:13:32 +0200

    added default values to fields

--- models/models.py
+++ models/models.py
@@ -1,36 +1,45 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from random import randint, choice
 
 
 class FootballClub(models.Model):
     """This model describes football club."""
     _inherit = 'account.analytic.line'
 
-    club_name = fields.Char(string="Club Name")
-    currency_id = fields.Many2one('res.currency', ondelete='set null', string="Currency")
-    annual_budget = fields.Monetary(currency_field="currency_id")
-    players_count = fields.Integer(string="Number of players")
+    club_name = fields.Char(string="Club Name", default="MyClub")
+    currency_id = fields.Many2one('res.currency', ondelete='set null', string="Currency",
+                                  default=lambda x: x.env.company.currency_id)
+    annual_budget = fields.Monetary(currency_field="currency_id", default=lambda x: randint(1, 10))
+    players_count = fields.Integer(string="Number of players", default=lambda x: len(x.env.company.user_ids))
     region = fields.Selection(
         selection=[
             ("Europe", "Europe"),
             ("Asia", "Asia"),
             ("Africa", "Africa"),
         ],
+        default="Asia",
         string="Region"
     )
-    year_created = fields.Date("Club created")
-    description = fields.Text(string="Club description")
-    is_active = fields.Boolean(string="Club is active?")
-    project_name_id = fields.One2many("project.project", "task_item_id", string="Project (O2m)")
+    year_created = fields.Date("Club created", default='2000-01-01')
+    description = fields.Text(string="Club description", default="Lorem ipsum")
+    is_active = fields.Boolean(string="Club is active?", compute="_check_if_active")
+    project_name_id = fields.One2many("project.project", "task_item_id", string="Project (O2m)", default=None)
+
+    def _check_if_active(self):
+        self.is_active = True
 
 
 class FootballPlayer(models.Model):
     """This model describes football club captain."""
     _inherit = 'account.analytic.line'
 
-    player_name = fields.Char(string="Captain Name")
-    club_id = fields.Many2one("res.partner", ondelete='set null', string="Captain previous club")
+    player_name = fields.Char(string="Captain Name", compute="_get_footballer_name")
+    club_id = fields.Many2one("res.partner", ondelete='set null', string="Captain previous club", default=2)
+
+    def _get_footballer_name(self):
+        self.player_name = choice(["Ronaldo", "Shevchenko", "Pele"])
 
 
 class ProjectInherited(models.Model):
@@ -38,4 +47,3 @@ class ProjectInherited(models.Model):
     _inherit = "project.project"
 
     task_item_id = fields.Many2one("account.analytic.line", ondelete='set null', string="Needed for O2M testing")
-
